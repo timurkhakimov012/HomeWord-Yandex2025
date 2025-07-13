72cba4de1ce426eac97012437b6ab248062e6d20
Timerkhan <khakimov012@yandex.ru>
2025-07-13 13:40:52 +0300

    zero

---
+++ src/client/components/CartBadge.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { cn } from '@bem-react/classname';
+import { useSelector } from 'react-redux';
+import { ApplicationState } from '../store';
+
+export interface CartBadgeProps {
+    id: number;
+}
+
+const bem = cn('CartBadge');
+
+export const CartBadge: React.FC<CartBadgeProps> = ({ id }) => {
+    const cart = useSelector((s: ApplicationState) => s.cart);
+
+    return <>
+        <div>1</div>
+        <div>2</div>
+        { cart[id] ? <span className={bem(null, ['text-success', 'mx-3'])}>Item in cart</span> : null}
+    </>
+}
---
+++ test/about.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { About } from '../src/client/pages/About';
+
+describe('About', () => {
+    it('Should render author name', () => {
+        const { container } = render(<About />);
+
+        expect(container.textContent).toBe('AboutAuthor: Malakhov');
+    });
+});
+
